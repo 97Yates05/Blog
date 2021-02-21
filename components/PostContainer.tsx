e794177f953108860c78549248e8997cf9c75de9
yangchenhui <t783072156@outlook.com>
2021-02-21 10:08:42 +0800

    :heavy_plus_sign: vditor替换marked依赖

--- components/PostContainer.tsx
+++ components/PostContainer.tsx
@@ -1,5 +1,8 @@
 import { gql, useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
+import marked from 'marked';
+import Vditor from 'vditor';
+import { useRef } from 'react';
 
 const GetPost = gql`
   query post($id: ID!) {
@@ -14,9 +17,14 @@ const GetPost = gql`
 export default () => {
   const router = useRouter();
   const { id } = router.query;
+  const container = useRef(null);
   const { loading, error, data } = useQuery(GetPost, { variables: { id } });
-
   if (loading) return <p>'Loading...'</p>;
+  if (data) {
+    Vditor.md2html(data.getpost.text).then((value) => {
+      container.current.innerHTML = value;
+    });
+  }
   if (error) return <p>Error! ${error.message}</p>;
   return (
     <div className="flex flex-col items-center">
@@ -27,11 +35,11 @@ export default () => {
       />
       <div className="text-2xl font-bold">{data.getpost.title}</div>
       <div>
-        {data.getpost.tags.map((tag) => {
-          return <span>{tag}</span>;
+        {data.getpost.tags.map((tag, index) => {
+          return <span key={index}>{tag}</span>;
         })}
       </div>
-      <div className="w-1/2">{data.getpost.text}</div>
+      <div className="w-1/2" ref={container} />
     </div>
   );
 };
